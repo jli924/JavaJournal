e57c748c2cbea98aacc3734fc6f2a19571b0ee78
jli924 <jjamiel1890@gmail.com>
2023-06-15 20:45:03 -0400

    Added popups for new event and task

--- src/main/java/cs3500/pa05/controller/JavaJournalController.java
+++ src/main/java/cs3500/pa05/controller/JavaJournalController.java
@@ -1,6 +1,8 @@
 package cs3500.pa05.controller;
 
 import java.io.File;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public interface JavaJournalController {
 
@@ -9,5 +11,4 @@ public interface JavaJournalController {
   public void saveToFile(File file);
 
   public void openFile(File file);
-
 }
