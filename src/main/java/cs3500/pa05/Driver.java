8c215534ad9031160cbb05b34f715df93c5bc70c
michellexc <129274881+michellexc@users.noreply.github.com>
2023-06-16 02:40:28 -0400

    Added custom application icon (will probably change later, im going to sleep)

--- src/main/java/cs3500/pa05/Driver.java
+++ src/main/java/cs3500/pa05/Driver.java
@@ -2,10 +2,12 @@ package cs3500.pa05;
 
 import cs3500.pa05.controller.JavaJournalController;
 import cs3500.pa05.controller.JavaJournalControllerImpl;
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import cs3500.pa05.view.JavaJournalView;
 import cs3500.pa05.view.JavaJournalViewImpl;
+import javafx.scene.image.Image;
 
 
 public class Driver extends Application {
@@ -22,11 +24,13 @@ public class Driver extends Application {
     JavaJournalView javaJournalView = new JavaJournalViewImpl(journalController);
     try {
       // load and place the cs3500.view's scene onto the stage
+      stage.getIcons().add(new Image(
+          getClass().getClassLoader().getResource("sky.jpg").openStream()));
       stage.setScene(javaJournalView.load());
       stage.setTitle("Java-Journal");
       stage.show();
       journalController.run();
-    } catch (IllegalStateException exc) {
+    } catch (IllegalStateException | IOException exc) {
       System.err.println("Unable to load GUI.");
     }
   }
